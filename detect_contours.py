d75d7f37d79b02cdb472548e55c86cd82e404b18
VTMC <wlaosxm@gmail.com>
2024-04-22 00:47:08 +0900

    add test image and change algorithm detect_contours.py, resizeImages.py

--- detect_contours.py
+++ detect_contours.py
@@ -6,17 +6,19 @@ def descendingOrderContours(contours):
 
     # contours.sort(key=lambda x: cv.contourArea(x), reverse=True)
 
-    max_limiter = min(len(sortedContours), 7)
-    sortedUpTo7SortedContours = []
+    # max_limiter = min(len(sortedContours), 7)
+    # sortedUpTo7SortedContours = []
 
-    for i in range(max_limiter):
-        area_value = cv.contourArea(sortedContours[i])
-        print("[descendingOrderContours] contour[{}]\'s area value : {}".format(i, area_value))
-        sortedUpTo7SortedContours.append(sortedContours[i])
+    # for i in range(max_limiter):
+    #     area_value = cv.contourArea(sortedContours[i])
+    #     print("[descendingOrderContours] contour[{}]\'s area value : {}".format(i, area_value))
+    #     sortedUpTo7SortedContours.append(sortedContours[i])
 
-    return sortedUpTo7SortedContours
+    # return sortedUpTo7SortedContours
 
-def detect_contours(img):
+    return sortedContours
+
+def detect_contours(windowName ,img):
     # Convert to gray scale
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
@@ -27,24 +29,54 @@ def detect_contours(img):
     contours, hierarchy = cv.findContours(adaptive_threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     # sort descending order contours
-    max_limiter_contours = descendingOrderContours(contours)
+    sorted_contours = descendingOrderContours(contours)
 
     # Draw contours
-    colors = [(255, 0, 0), (255, 128, 0), (255, 255, 0), (0, 255, 0), (0, 0, 255), (0, 0, 128), (255, 0, 255)]
+    colors = [(255, 0, 0), (255, 128, 0), (255, 255, 0), (0, 255, 0), (0, 0, 255), (0, 0, 128), (255, 0, 255),
+    (0, 0, 0), (128,128,128), (200, 200, 200)]
 
-    cv.drawContours(img, [max_limiter_contours], -1, (0, 255, 0), 2)
+    for i in range(len(colors)):
+        cv.drawContours(img, sorted_contours, i, colors[i], 3, cv.LINE_8, hierarchy)
+        print("sorted contours [%d]'s contours Area : %f" % (i, cv.contourArea(sorted_contours[i])))
 
-    cv.namedWindow('detected contours', cv.WINDOW_NORMAL)
-    cv.imshow("detected contours", img)
-    cv.resizeWindow("detected contours", 500, 800)
+    image_processed_res_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-    # return max_limiter_contours
+    # cv.namedWindow(windowName+'\'s detected contours', cv.WINDOW_NORMAL)
+    # cv.imshow(windowName+'\'s detected contours', image_processed_res_img)
+    # cv.resizeWindow(windowName+'\'s detected contours', 500, 800)
+
+    return image_processed_res_img
 
 # def cropImgByContour(img, contour):
 
-img = cv.imread("C:/Users/owner/Downloads/testingImages/t1_1.jpg")
+path = "K:/python/traceUrine_NewAlgorithm/testingImages/"
 
-detectedContours = detect_contours(img)
+img_t1_1 = cv.imread(path+"resized_1_1.jpg")
+img_t3_4 = cv.imread(path+"resized_3_4.jpg")
+img_t9_16 = cv.imread(path+"resized_9_16.jpg")
+img_t_full = cv.imread(path+"resized_full.jpg")
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+# resized_t1_1 = cv.resize(img_t1_1, (1000, 2000))
+# print("t1_1 resized")
+# resized_t3_4 = cv.resize(img_t3_4, (1000,2000))
+# print("t3_4 resized")
+# resized_t9_16 = cv.resize(img_t9_16, (1000, 2000))
+# print("t9_16 resized")
+# resized_t_full = cv.resize(img_t_full, (1000, 2000))
+# print("t_full resized")
+
+print("t1_1 test...")
+detectedContours_t1_1 = detect_contours("img_t1_1",img_t1_1)
+print("\n\nt3_4 test...")
+detectedContours_t3_4 = detect_contours("img_t3_4",img_t3_4)
+print("\n\nt9_16 test...")
+detectedContours_t9_16 = detect_contours("img_t9_16",img_t9_16)
+print("\n\nt_full test...")
+detectedContours_t_full = detect_contours("img_t_full",img_t_full)
+
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+cv.imwrite(path+"contour_1_1.jpg", detectedContours_t1_1)
+cv.imwrite(path+"conotur_3_4.jpg", detectedContours_t3_4)
+cv.imwrite(path+"contour_9_16.jpg", detectedContours_t9_16)
+cv.imwrite(path+"contour_full.jpg", detectedContours_t_full)
--- resizeImages.py
+++ resizeImages.py
@@ -1,33 +1,42 @@
 import cv2 as cv
 import numpy as np
 
-def resizeImgWithRatio(img, target_width, target_height):
+def resizeImgWithRatio(img, target_height):
     width = img.shape[1]
     height = img.shape[0]
-    print("[resizeImgWithRatio] img width : ", width, " / img height : ", height)
+    print("[resizeImgWithRatio] img width : %d, height %d" % (width, height))
+
+    height_ratio = target_height / height
+    new_width = width * height_ratio
+
+    resized_img = cv.resize(img, (int(new_width), target_height))
+
+    return resized_img
+
+def resizeToSqrImgWithRatio(img, target_width, target_height):
+    resultMat = np.zeros((target_width, target_height,3), np.uint8)
+    
+    width = img.shape[1]
+    height = img.shape[0]
+    print("[resizeToSqrImgWithRatio] img width : ", width, " / img height : ", height)
+    print("ratio of width, height : %f" % (width/height))
 
     width_ratio = target_width / width
     height_ratio = target_height / height
-    print("[resizeImgWithRatio] img width ratio : ", width_ratio, " / img height ratio : ", height_ratio)
+    print("[resizeToSqrImgWithRatio] img width ratio : ", width_ratio, " / img height ratio : ", height_ratio)
 
-    ratio_fit_width = width * width_ratio
-    ratio_fit_height = height * height_ratio
-    print("[resizeImgWithRatio] img fit ratio width : ", ratio_fit_width, " / img fit ratio height: ", ratio_fit_height)
-
-    if(width_ratio > 1): # img width > target_width
-        
-    elif(width_ratio < 1):
+    if width_ratio < height_ratio:
+        ratio_fit_size = (int(width*width_ratio), int(height*width_ratio))  
     else:
+        ratio_fit_size = (int(width*height_ratio), int(height*height_ratio))
 
+    ratio_fit_img = cv.resize(img, ratio_fit_size)
+    print("resized ratio of width, height : %f" % (ratio_fit_img.shape[1] / ratio_fit_img.shape[0]))
 
+    resultMat[int(target_height/2 - ratio_fit_size[1]/2) : int(target_height/2 + ratio_fit_size[1]/2),
+    int(target_width/2 - ratio_fit_size[0]/2) : int(target_width/2 + ratio_fit_size[0]/2), :] = ratio_fit_img
 
-    # ratio_fit_resized_img = cv.resize(img, (int(ratio_fit_width), int(ratio_fit_height)), interpolation=cv.INTER_LINEAR)
-
-    ratio_fit_resized_img = cv.resize(img, dsize=(0,0), fx=width_ratio, fy=height_ratio)
-
-    # resized_img = cv.resize(ratio_fit_resized_img, (target_width, target_height))
-
-    return ratio_fit_resized_img
+    return resultMat
 
 def cropImg(img, cropWidth, cropHeight):
     img_height, img_width = img.shape[:2]
@@ -45,10 +54,17 @@ def cropImg(img, cropWidth, cropHeight):
     return cropped_image
 
 
-img1_1 = cv.imread("C:/Users/owner/Downloads/testingImages/1_1.jpg")
-img3_4 = cv.imread("C:/Users/owner/Downloads/testingImages/3_4.jpg")
-img9_16 = cv.imread("C:/Users/owner/Downloads/testingImages/9_16.jpg")
-img_full = cv.imread("C:/Users/owner/Downloads/testingImages/full.jpg")
+path = "K:/python/traceUrine_NewAlgorithm/testingImages/"
+
+img1_1 = cv.imread(path+"29_5_t1_1.jpg")
+img3_4 = cv.imread(path+"29_5_t3_4.jpg")
+img9_16 = cv.imread(path+"29_5_t9_16.jpg")
+img_full = cv.imread(path+"29_5_t_full.jpg")
+
+# img1_1 = cv.imread(path+"t1_1.jpg")
+# img3_4 = cv.imread(path+"t3_4.jpg")
+# img9_16 = cv.imread(path+"t9_16.jpg")
+# img_full = cv.imread(path+"t_full.jpg")
 
 # check width, height
 print("target size : 2000*4000") #width < height -> ratio = 0.5
@@ -64,11 +80,17 @@ print("img_full size : ", img_full.shape)
 # src_img_full = np.array([[0, 0], [img_full.shape[1], 0], [img_full.shape[1], img_full.shape[0]], [0, img_full.shape[0]]], dtype =np.float32)
 # dst_img_point = np.array([[0,0], [500, 0], [500, 1000], [0, 1000]], dtype= np.float32)
 
+# resize image by img's height
+resized_img1_1 = resizeImgWithRatio(img1_1, 4000)
+resized_img3_4 = resizeImgWithRatio(img3_4, 4000)
+resized_img9_16 = resizeImgWithRatio(img9_16, 4000)
+resized_img_full = resizeImgWithRatio(img_full, 4000)
+
 # fit ratio with resize images
-resized_img1_1 = resizeImgWithRatio(img1_1, 1000, 2000)
-resized_img3_4 = resizeImgWithRatio(img3_4, 1000, 2000)
-resized_img9_16 = resizeImgWithRatio(img9_16, 1000, 2000)
-resized_img_full = resizeImgWithRatio(img_full, 1000, 2000)
+resized_sqr_img1_1 = resizeToSqrImgWithRatio(resized_img1_1, 2000, 2000)
+resized_sqr_img3_4 = resizeToSqrImgWithRatio(resized_img3_4, 2000, 2000)
+resized_sqr_img9_16 = resizeToSqrImgWithRatio(resized_img9_16, 2000, 2000)
+resized_sqr_img_full = resizeToSqrImgWithRatio(resized_img_full, 2000, 2000)
 
 # get matrix to transform
 # matrix1_1 = cv.getPerspectiveTransform(src_img1_1, dst_img_point)
@@ -88,7 +110,7 @@ resized_img_full = resizeImgWithRatio(img_full, 1000, 2000)
 # resized_img9_16 = cropImg(img9_16, 1000, 2000)
 # resized_img_full = cropImg(img_full, 1000, 2000)
 
-cv.imwrite("C:/Users/owner/Downloads/testingImages/resized_1_1.jpg", resized_img1_1)
-cv.imwrite("C:/Users/owner/Downloads/testingImages/resized_3_4.jpg", resized_img3_4)
-cv.imwrite("C:/Users/owner/Downloads/testingImages/resized_9_16.jpg", resized_img9_16)
-cv.imwrite("C:/Users/owner/Downloads/testingImages/resized_full.jpg", resized_img_full)
+cv.imwrite(path+"resized_1_1.jpg", resized_sqr_img1_1)
+cv.imwrite(path+"resized_3_4.jpg", resized_sqr_img3_4)
+cv.imwrite(path+"resized_9_16.jpg", resized_sqr_img9_16)
+cv.imwrite(path+"resized_full.jpg", resized_sqr_img_full)
